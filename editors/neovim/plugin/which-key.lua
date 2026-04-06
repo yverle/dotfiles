f1581e0be0a97b351e2e5d4c4b7a981b02881c55
Yenti Verlé <yenti.verle@outlook.com>
2026-04-06 18:05:46 +0200

    fix(which-key): remove vim.g.have_nerd_font reference

--- editors/neovim/plugin/which-key.lua
+++ editors/neovim/plugin/which-key.lua
@@ -3,43 +3,11 @@
 vim.pack.add { 'https://github.com/folke/which-key.nvim' }
 
 require('which-key').setup {
-  -- delay between pressing a key and opening which-key (milliseconds)
-  -- this setting is independent of vim.o.timeoutlen
   delay = 0,
   icons = {
-    mappings = vim.g.have_nerd_font,
-    -- If you are using a Nerd Font: set icons.keys to an empty table which will use the
-    -- default which-key.nvim defined Nerd Font icons, otherwise define a string table
-    keys = vim.g.have_nerd_font and {} or {
-      Up = '<Up> ',
-      Down = '<Down> ',
-      Left = '<Left> ',
-      Right = '<Right> ',
-      C = '<C-…> ',
-      M = '<M-…> ',
-      D = '<D-…> ',
-      S = '<S-…> ',
-      CR = '<CR> ',
-      Esc = '<Esc> ',
-      ScrollWheelDown = '<ScrollWheelDown> ',
-      ScrollWheelUp = '<ScrollWheelUp> ',
-      NL = '<NL> ',
-      BS = '<BS> ',
-      Space = '<Space> ',
-      Tab = '<Tab> ',
-      F1 = '<F1>',
-      F2 = '<F2>',
-      F3 = '<F3>',
-      F4 = '<F4>',
-      F5 = '<F5>',
-      F6 = '<F6>',
-      F7 = '<F7>',
-      F8 = '<F8>',
-      F9 = '<F9>',
-      F10 = '<F10>',
-      F11 = '<F11>',
-      F12 = '<F12>',
-    },
+    mappings = true,
+    -- Default which-key.nvim defined Nerd Font icons
+    keys = {},
   },
 
   -- Document existing key chains
@@ -47,6 +15,5 @@ require('which-key').setup {
     { '<leader>s', group = '[S]earch' },
     { '<leader>t', group = '[T]oggle' },
     { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-    { '<leader>g', group = '[G]rapple' },
   },
 }
