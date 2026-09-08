9fcf84972e41d73500c5396dad84ec883b56888e
Yenti Verlé <yenti.verle@outlook.com>
2026-09-08 13:57:39 +0200

    feat(nvim): add spell to text based filetypes

--- packages/neovim/ftplugin/org.lua
+++ packages/neovim/ftplugin/org.lua
@@ -1,1 +1,2 @@
 vim.wo.wrap = true
+vim.wo.spell = true
--- packages/neovim/plugin/org.lua
+++ packages/neovim/plugin/org.lua
@@ -4,4 +4,10 @@ require("orgmode").setup({
     org_agenda_files = "~/org/**/*",
     org_default_notes_file = "~/org/refile.org",
     org_startup_indented = true,
+    mappings = {
+        org = {
+            org_cycle = { "gt", desc = "cycle fold" },
+            org_global_cycle = { "gT", desc = "cycle fold global" },
+        },
+    },
 })
